30b573e98a5e0d2592fc3e537540f685a711c3d7
MeGallin <me@garyallin.uk>
2024-02-27 10:00:27 +0000

    Capcitor and Ionic setup

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,5 +1,24 @@
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+} from '@ionic/react';
+
 const Dice = () => {
-  return <div>Dice</div>;
+  return (
+    <div>
+      <IonCard>
+        <IonCardHeader>
+          <IonCardTitle>Speed Dice</IonCardTitle>
+          <IonCardSubtitle>Upto 6 times quicker</IonCardSubtitle>
+        </IonCardHeader>
+
+        <IonCardContent>DICE to follow</IonCardContent>
+      </IonCard>
+    </div>
+  );
 };
 
 export default Dice;
