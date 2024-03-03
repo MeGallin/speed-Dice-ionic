77150eb8e4bf2937ef59e3ac36cfe02c1351f57c
MeGallin <me@garyallin.uk>
2024-03-03 16:26:34 +0000

    Move hook to hooks folder

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useDiceRoller } from '../hooks/useDiceRoller';
 import {
   IonButton,
   IonCard,
@@ -11,59 +11,19 @@ import {
 import './Dice.css';
 import { arrowForwardOutline } from 'ionicons/icons';
 
-const rollDice = () => Math.floor(Math.random() * 6) + 1;
-
-const useDiceRoller = (initialValue: number | null = null) => {
-  const [values, setValues] = useState<Array<number | null>>([
-    initialValue,
-    initialValue,
-    initialValue,
-  ]);
-  const [message, setMessage] = useState<string>(
-    'Please pass the dice to the next person',
-  );
-  const [spinCount, setSpinCount] = useState<number>(0); // State for counting spins
-  const [totals, setTotals] = useState<Array<number>>([]); // State for storing totals of each spin
-
-  const spin = () => {
-    setSpinCount(spinCount + 1);
-    const newValues = values.map(() => rollDice());
-    setValues(newValues);
-
-    const frequencyCounter: Record<number, number> = {};
-    for (const item of newValues) {
-      frequencyCounter[item] = (frequencyCounter[item] || 0) + 1;
-    }
-
-    let foundDuplicate = false;
-    let foundTriplicate = false;
-    for (const count of Object.values(frequencyCounter)) {
-      if (count === 3) {
-        foundTriplicate = true;
-        break; // Exit the loop early if a triplicate is found
-      } else if (count === 2) {
-        foundDuplicate = true;
-        // Don't break here because we need to check if there's a triplicate
-      }
-    }
-
-    if (foundTriplicate) {
-      setMessage('Triple, please move to home and spin again');
-    } else if (foundDuplicate) {
-      setMessage('Double, please spin again');
-    } else {
-      setMessage('Please pass the dice to the next person');
-    }
-
-    const total = newValues.reduce((acc, value) => acc + (value ?? 0), 0);
-    setTotals([...totals, total]); // Add the new total to the totals array
-  };
-
-  return { values, spin, message, spinCount, totals };
-};
+useDiceRoller;
 
 const Dice = () => {
   const { values, spin, message, spinCount, totals } = useDiceRoller();
+  // Define an array of animation classes
+  const animations = [
+    'bounce',
+    'rotateIn',
+    'bounceInRight',
+    'fadeInLeftBig',
+    'bounceInUp',
+    'fadeIn',
+  ];
   return (
     <div>
       <IonCard>
@@ -74,7 +34,9 @@ const Dice = () => {
                 {value && (
                   <IonImg
                     src={`../assets/${value}.png`}
-                    className="animate__animated animate__bounce"
+                    className={`animate__animated animate__${
+                      animations[value - 1]
+                    }`}
                   ></IonImg>
                 )}
               </span>
@@ -86,10 +48,10 @@ const Dice = () => {
           <div>{message}</div>
           <div>
             Please move forward by {totals[totals.length - 1] || 0} spaces
-          </div>{' '}
-          {/* Display the last total */}
+          </div>
         </IonCardContent>
       </IonCard>
+
       <IonCard>
         <IonCardContent>
           <IonButton
@@ -103,7 +65,15 @@ const Dice = () => {
           </IonButton>
           <div>Spin Count: {spinCount}</div>{' '}
           {/* Display the number of times spin was called */}
-          <div>Totals: {totals.map((total, index) => `{${index + 1}}${total}`).join(', ')}</div>
+        </IonCardContent>
+      </IonCard>
+
+      <IonCard>
+        <IonCardContent>
+          <div>
+            Totals:{' '}
+            {totals.map((total, index) => `{${index + 1}}${total}`).join(', ')}
+          </div>
           {/* Display the totals array */}
         </IonCardContent>
       </IonCard>
---
+++ src/hooks/useDiceRoller.ts
@@ -0,0 +1,52 @@
+
+import { useState } from 'react';
+const rollDice = () => Math.floor(Math.random() * 6) + 1;
+
+export const useDiceRoller = (initialValue: number | null = null) => {
+  const [values, setValues] = useState<Array<number | null>>([
+    initialValue,
+    initialValue,
+    initialValue,
+  ]);
+  const [message, setMessage] = useState<string>(
+    'Please pass the dice to the next person',
+  );
+  const [spinCount, setSpinCount] = useState<number>(0); // State for counting spins
+  const [totals, setTotals] = useState<Array<number>>([]); // State for storing totals of each spin
+
+  const spin = () => {
+    setSpinCount(spinCount + 1);
+    const newValues = values.map(() => rollDice());
+    setValues(newValues);
+
+    const frequencyCounter: Record<number, number> = {};
+    for (const item of newValues) {
+      frequencyCounter[item] = (frequencyCounter[item] || 0) + 1;
+    }
+
+    let foundDuplicate = false;
+    let foundTriplicate = false;
+    for (const count of Object.values(frequencyCounter)) {
+      if (count === 3) {
+        foundTriplicate = true;
+        break; // Exit the loop early if a triplicate is found
+      } else if (count === 2) {
+        foundDuplicate = true;
+        // Don't break here because we need to check if there's a triplicate
+      }
+    }
+
+    if (foundTriplicate) {
+      setMessage('Triple, please move to home and spin again');
+    } else if (foundDuplicate) {
+      setMessage('Double, please spin again');
+    } else {
+      setMessage('Please pass the dice to the next person');
+    }
+
+    const total = newValues.reduce((acc, value) => acc + (value ?? 0), 0);
+    setTotals([...totals, total]); // Add the new total to the totals array
+  };
+
+  return { values, spin, message, spinCount, totals };
+};
