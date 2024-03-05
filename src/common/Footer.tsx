849034ca21b2b33ebbbfabc6e46d670dcd8896a2
MeGallin <me@garyallin.uk>
2024-03-05 14:18:57 +0000

    DateTime comp

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,5 +1,16 @@
-const footer = () => {
-  return <footer>footer</footer>;
+import { IonFooter, IonToolbar, IonTitle } from '@ionic/react';
+import DateTime from '../components/dateTime/DateTime';
+
+const Footer = () => {
+  return (
+    <IonFooter>
+      <IonToolbar>
+        <IonTitle size="small" class="ion-text-center">
+          <DateTime />
+        </IonTitle>
+      </IonToolbar>
+    </IonFooter>
+  );
 };
 
-export default footer;
+export default Footer;
